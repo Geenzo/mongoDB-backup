1ace6c342f79a31825fedf5cd5c6b1f29914073a
Darren Hay <darrenhay1994@gmail.com>
2019-03-03 15:26:23 +0000

    add: basic import process

---
+++ import.ts
@@ -0,0 +1,39 @@
+import * as mongoose from 'mongoose';
+import * as fs from 'fs';
+
+const mongoString: string = 'mongodb://localhost/test2'
+
+const getImportData = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./3-3-2019_backup.json', { encoding: 'utf8' }, (err, data) => {
+            if (err) throw err;
+            return resolve(JSON.parse(data));
+        })
+    })
+}
+
+const importData = (connection, data) => {
+    const finishedImport = data.map(collection => {
+        const schema = new mongoose.Schema({}, { strict: false });
+        const collectionModel = connection.model(collection.collectionName, schema, collection.collectionName);
+
+        return collection.collectionData.map(item => {
+            return new collectionModel(item).save();
+        });
+    })
+    return Promise.all(finishedImport);
+}
+
+mongoose.connect(mongoString, { useNewUrlParser: true }).then(async (connection) => {
+    try {
+        const getData = await getImportData();
+        const importedData = await importData(connection, getData);
+    
+        // TODO: exit node process correctly when successful import
+        return importedData;
+    } catch(error) {
+        // FIXME: handle error where duplicate ID's from improting same file.
+        console.log('Error:', error);
+        process.exit(1);
+    }
+})
