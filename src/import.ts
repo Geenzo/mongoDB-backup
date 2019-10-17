ac7f6cea57fb151138bce511ed626377cf7f0414
Darren Hay <darrenhay1994@gmail.com>
2019-10-18 00:00:27 +0100

    refactor: changing scripts to export nad import

--- src/import.ts
+++ src/import.ts
@@ -1,20 +1,20 @@
 import * as fs from "fs";
-import * as mongoose from "mongoose";
+import { connect, Connection, Schema } from "mongoose";
 
-const mongoString: string = "mongodb://localhost:27017/admin";
+const mongoString: string = "mongodb://localhost:27017/test2";
 
 const getImportData = () => {
     return new Promise((resolve, reject) => {
         fs.readFile("./3-3-2019_backup.json", { encoding: "utf8" }, (err, data) => {
             if (err) { throw err; }
             return resolve(JSON.parse(data));
-        })
+        });
     });
 };
 
-const importData = (connection, data) => {
+const importData = (connection: Connection, data) => {
     const finishedImport = data.map((collection) => {
-        const schema = new mongoose.Schema({}, { strict: false });
+        const schema = new Schema({}, { strict: false });
         const collectionModel = connection.model(collection.collectionName, schema, collection.collectionName);
 
         return collection.collectionData.map((item) => {
@@ -24,14 +24,14 @@ const importData = (connection, data) => {
     return Promise.all(finishedImport);
 };
 
-mongoose.connect(mongoString, { useNewUrlParser: true }).then(async (connection) => {
+connect(mongoString, { useNewUrlParser: true }).then(async (connection: Connection) => {
     try {
         const getData = await getImportData();
         const importedData = await importData(connection, getData);
 
         // TODO: exit node process correctly when successful import
         return importedData;
-    } catch(error) {
+    } catch (error) {
         // FIXME: handle error where duplicate ID's from improting same file.
         console.log("Error:", error);
         process.exit(1);
